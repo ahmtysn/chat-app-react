e3e8ab21b58131e13b3e83a91570f7a42d6140bb
ahmtysn <ahmtysn14@gmail.com>
2020-11-03 08:16:01 +0100

    style 100% page

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -2,37 +2,34 @@ import React, { useState, useEffect } from 'react';
 import { Grid, Divider, Box, makeStyles } from '@material-ui/core';
 import queryString from 'query-string';
 import io from 'socket.io-client';
-
 import '../App.css';
-
 import Header from './Header';
 import SideBarInfo from './SideBarInfo';
 import Messages from './Messages';
 import Input from './Input';
-
 const ENDPOINT = 'https://react-chat-ayk.herokuapp.com/'; // localhost:5000
-
 let socket;
-
 const useStyles = makeStyles(theme => ({
   root: {
+    display: 'flex',
+    flexDirection: 'column',
     height: '100%',
   },
   chatContainer: {
-    height: '100%',
-    '& > div': {
-      height: '80%',
-    },
+    flex: 1,
   },
-  chatMessage: {
+  chats: {
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'space-between',
     backgroundColor: '#132743',
-    color: '#edc988',
+    color: '#EDC988',
+  },
+  messages: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1,
   },
 }));
-
 const Chat = ({ location }) => {
   const classes = useStyles();
   const [name, setName] = useState('');
@@ -40,55 +37,50 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
-
     socket = io(ENDPOINT);
-
     setRoom(room);
     setName(name);
-
     socket.emit('join', { name, room }, error => {
       if (error) {
         alert(error);
       }
     });
   }, [ENDPOINT, location.search]);
-
   useEffect(() => {
     socket.on('message', message => {
       setMessages(messages => [...messages, message]);
     });
-
     socket.on('roomData', ({ users }) => {
       setUsers(users);
     });
   }, []);
-
   const sendMessage = event => {
     event.preventDefault();
-
     if (message) {
       socket.emit('sendMessage', message, () => setMessage(''));
     }
   };
-
   return (
     <div className={classes.root}>
       <Header name={name} room={room} />
-      <Grid container className={classes.chatContainer}>
+      <Grid container alignItems='stretch' className={classes.chatContainer}>
         <Box
           component={Grid}
           item
           sm={3}
           display={{ xs: 'none', sm: 'block' }}
-          style={{ backgroundColor: '#edc988' }}
+          style={{ backgroundColor: '#EDC988', flex: 1 }}
         >
-          <SideBarInfo users={users} style={{ height: '100vh' }} />
+          <SideBarInfo users={users} />
         </Box>
-        <Grid item xs={12} sm={9} className={classes.chatMessage}>
-          <Messages messages={messages} name={name} />
+        <Grid item xs={12} sm={9} className={classes.chats}>
+          <Messages
+            messages={messages}
+            name={name}
+            className={classes.messages}
+          />
           <Input
             message={message}
             setMessage={setMessage}
@@ -99,5 +91,4 @@ const Chat = ({ location }) => {
     </div>
   );
 };
-
 export default Chat;
--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,11 +1,8 @@
 import React from 'react';
-
 import ScrollToBottom from 'react-scroll-to-bottom';
-
 import Message from './Message';
-
-const Messages = ({ messages, name }) => (
-  <ScrollToBottom>
+const Messages = ({ messages, name, className }) => (
+  <ScrollToBottom className={className}>
     {messages.map((message, i) => (
       <div key={i} style={{ padding: '10px' }}>
         <Message message={message} name={name} />
@@ -13,5 +10,4 @@ const Messages = ({ messages, name }) => (
     ))}
   </ScrollToBottom>
 );
-
 export default Messages;
